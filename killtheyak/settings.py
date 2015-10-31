d8d3212c92d1300bbf9b74080069a52242f32350
Adam Schwartz <adam@adamschwartz.io>
2015-10-31 12:21:02 +0530

    updating killtheyak to live in my own repo

--- killtheyak/settings.py
+++ killtheyak/settings.py
@@ -22,7 +22,7 @@ FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite', 'fenced_code', 'footnotes']
 FLATPAGES_ROOT = os.path.join(PROJECT_ROOT, 'killtheyak-pages')
 FLATPAGES_EXTENSION = '.md'
 
-BASE_URL = 'http://killtheyak.com'
-GITHUB_REPO = 'http://www.github.com/killtheyak/killtheyak.github.io'
-PAGES_REPO = 'http://www.github.com/killtheyak/killtheyak-pages'
-RAW_PAGES_PREFIX = "https://raw.github.com/killtheyak/killtheyak-pages/master/"
+BASE_URL = 'http://daschwa.github.io/killtheyak'
+GITHUB_REPO = 'http://www.github.com/daschwa/killtheyak'
+PAGES_REPO = 'http://www.github.com/daschwa/killtheyak-pages'
+RAW_PAGES_PREFIX = "https://raw.github.com/daschwa/killtheyak-pages/master/"
